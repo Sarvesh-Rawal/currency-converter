5077cd70a7d112d49413ea79e7d09d543de02243
Sarvesh Rawal <sarveshrawal.2006@gmail.com>
2024-07-19 16:20:41 +0530

    added exchnage currency codes feature

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let toImg = document.querySelector('.to img');
 let dropdowns = document.querySelectorAll('.dropdown select');
 let btn = document.querySelector('form button');
 let msg = document.querySelector('.msg-container p');
-let changeImg = document.querySelector('i');
+let exchangeImg = document.querySelector('i');
+let fromCurrVal, toCurrVal;
 
 const updateFlag = () => {
     let currFromCountry = countryList[fromcurr.value]; 
@@ -43,27 +44,43 @@ const exchangeRate = async () => {
         amtval = 1;
     }
 
-    let fromCurrVal = fromcurr.value;
-    let toCurrVal = tocurr.value;
-    console.log(fromCurrVal,toCurrVal);
-    
-    const baseUrl = `https://api.currencyapi.com/v3/latest?apikey=cur_live_NS3pR18wUMkrxAVrqz5PAbaGwOhil2eM7PzIttxR&base_currency=${fromCurrVal}`;
-    console.log(baseUrl);
+    fromCurrVal = fromcurr.value;
+    toCurrVal = tocurr.value;
 
-    const URL = `${baseUrl}=${fromCurrVal}`;
+    const baseUrl = `https://api.currencyapi.com/v3/latest?apikey=cur_live_NS3pR18wUMkrxAVrqz5PAbaGwOhil2eM7PzIttxR&base_currency=${fromCurrVal}`;
+
     let response = await fetch(baseUrl);
     let data = await response.json();
     let exchangeValue = data['data'][toCurrVal]['value'];
-    console.log(exchangeValue);
-    console.log(amtval);
+
     let finalAmt = (amtval * exchangeValue).toFixed(2);
-    console.log(finalAmt);
     msg.classList.remove('hide');
     msg.innerText = `${amtval} ${fromCurrVal} = ${finalAmt} ${toCurrVal}`;
-
 }
 
 btn.addEventListener('click',(evt) => {
     evt.preventDefault();
     exchangeRate();
 })  
+
+const exchangevalues = () => {
+    let temp = fromCurrVal;
+    let fromSelect = document.querySelector('.from select');
+    let toSelect = document.querySelector('.to select');
+    let newdrop1 = document.createElement('option');
+    let newdrop2 = document.createElement('option');
+
+    newdrop1.value = toCurrVal;
+    newdrop1.innerText = toCurrVal;
+    newdrop1.selected = 'selected';
+    fromSelect.append(newdrop1);
+
+    newdrop2.value = temp;
+    newdrop2.innerText = temp;
+    newdrop2.selected = 'selected';
+    toSelect.append(newdrop2);
+
+    updateFlag();
+}
+
+exchangeImg.addEventListener('click',exchangevalues);
